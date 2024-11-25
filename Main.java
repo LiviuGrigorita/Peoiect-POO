bd070b87cd368026e9ac2eb80530ef36e42b5656
LiviuGrigorita <liviugaby1@gmail.com>
2024-11-25 15:14:33 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,24 @@
-public class Main {
+package com.mycompany.proiect_java;
+
+/**
+ *
+ * @author jh0nix
+ */
+public class Proiect_java {
+
     public static void main(String[] args) {
-        //obiect Lampa
-        Lampa lampa = new Lampa(true, 75, "galben", 12.5, "LED");
-        System.out.println(lampa);
-        
-        //obiect LampaSmartInterior
-        LampaSmartInterior lampaSmart = new LampaSmartInterior(true, 90, "RGB", 15.0, "Smart LED", true, "Phillips");
-        System.out.println(lampaSmart);
-        
-        //Obiect Prelungitor
-        Prelungitor prelungitor = new Prelungitor(5, true);
-        System.out.println(prelungitor);
+        Lampa l1= new Lampa();
+        System.out.println(l1);
+        LampaExterioara e1=new LampaExterioara();
+        System.out.println(e1);
+        SursaIluminat s1=new SursaIluminat();
+        System.out.println(s1);
+         LampaExterioara lampaExterioara = new LampaExterioara();
+         lampaExterioara.setrezistent_apa(true);
+         System.out.println(lampaExterioara);
+         lampaExterioara.porneste();
+         l1.porneste();
+        System.out.println("este pornita? "+l1.ONsauOFF());
+        System.out.println("este pornita? "+lampaExterioara.ONsauOFF());
     }
 }

bac754e7a3999c291042fe047ccca055a03176a5
LiviuGrigorita <liviugaby1@gmail.com>
2024-11-25 15:26:34 +0200

    Prelungitor.java

--- Prelungitor.java
+++ Prelungitor.java
@@ -1,27 +1,90 @@
-public class Prelungitor {
+package com.mycompany.proiect_java;
+
+public class Prelungitor extends Lampa {
     private int numarPrize;
-    private boolean areProtectieSupratensiune;
-    
-    //constructor fara argumente 
+    private double lungimeCablu;
+    private boolean protectieSupratensiune;
+    private String tipConector;
+    private int putereMaxima;
+
     public Prelungitor() {
+        super();
         this.numarPrize = 0;
-        this.areProtectieSupratensiune = false;
+        this.lungimeCablu = 0.0;
+        this.protectieSupratensiune = false;
+        this.tipConector = "necunoscut";
+        this.putereMaxima = 0;
     }
-    //constructor cu toate argumentele
-    public Prelungitor(int numarPrize, boolean areProtectieSupratensiune) {
+
+    public Prelungitor(int numarPrize, double lungimeCablu, boolean protectieSupratensiune, String tipConector, int putereMaxima, int marime_bec, String serie, int inaltime, String culoare, boolean cuBaterie) {
+        super(marime_bec, serie, inaltime, culoare, cuBaterie);
         this.numarPrize = numarPrize;
-        this.areProtectieSupratensiune = areProtectieSupratensiune;
+        this.lungimeCablu = lungimeCablu;
+        this.protectieSupratensiune = protectieSupratensiune;
+        this.tipConector = tipConector;
+        this.putereMaxima = putereMaxima;
     }
-    //constructor de copiere
-    public Prelungitor(Prelungitor altPrelungitor) {
-        this.numarPrize = altPrelungitor.numarPrize;
-        this.areProtectieSupratensiune = altPrelungitor.areProtectieSupratensiune;
+
+    public Prelungitor(Prelungitor obj) {
+        super(obj);
+        if (obj != null) {
+            this.numarPrize = obj.numarPrize;
+            this.lungimeCablu = obj.lungimeCablu;
+            this.protectieSupratensiune = obj.protectieSupratensiune;
+            this.tipConector = obj.tipConector;
+            this.putereMaxima = obj.putereMaxima;
         }
-    //rescrierea metodei toString
+    }
+
+    public void setNumarPrize(int numarPrize) {
+        this.numarPrize = numarPrize;
+    }
+
+    public int getNumarPrize() {
+        return this.numarPrize;
+    }
+
+    public void setLungimeCablu(double lungimeCablu) {
+        this.lungimeCablu = lungimeCablu;
+    }
+
+    public double getLungimeCablu() {
+        return this.lungimeCablu;
+    }
+
+    public void setProtectieSupratensiune(boolean protectieSupratensiune) {
+        this.protectieSupratensiune = protectieSupratensiune;
+    }
+
+    public boolean getProtectieSupratensiune() {
+        return this.protectieSupratensiune;
+    }
+
+    public void setTipConector(String tipConector) {
+        this.tipConector = tipConector;
+    }
+
+    public String getTipConector() {
+        return this.tipConector;
+    }
+
+    public void setPutereMaxima(int putereMaxima) {
+        this.putereMaxima = putereMaxima;
+    }
+
+    public int getPutereMaxima() {
+        return this.putereMaxima;
+    }
 
     @Override
     public String toString() {
-        return "Prelungitor{" + "numarPrize=" + numarPrize + ", areProtectieSupratensiune=" + areProtectieSupratensiune + '}';
+        return "Prelungitor{" +
+                "numarPrize=" + numarPrize +
+                ", lungimeCablu=" + lungimeCablu +
+                ", protectieSupratensiune=" + protectieSupratensiune +
+                ", tipConector='" + tipConector + '\'' +
+                ", putereMaxima=" + putereMaxima +
+                ", " + super.toString() +
+                '}';
     }
-    
 }

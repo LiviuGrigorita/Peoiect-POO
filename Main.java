dc7ba08c195630d88e7c2c8369e95cc81e2cb268
LiviuGrigorita <liviugaby1@gmail.com>
2024-11-25 15:33:19 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,34 @@
 package com.mycompany.proiect_java;
 
-/**
- *
- * @author jh0nix
- */
 public class Proiect_java {
 
     public static void main(String[] args) {
         Lampa l1= new Lampa();
         System.out.println(l1);
-        LampaExterioara e1=new LampaExterioara();
+        LampaInterioara e1=new LampaInterioara();
         System.out.println(e1);
-        SursaIluminat s1=new SursaIluminat();
+        Prelungitor s1=new Prelungitor();
         System.out.println(s1);
-         LampaExterioara lampaExterioara = new LampaExterioara();
-         lampaExterioara.setrezistent_apa(true);
-         System.out.println(lampaExterioara);
-         lampaExterioara.porneste();
-         l1.porneste();
-        System.out.println("este pornita? "+l1.ONsauOFF());
-        System.out.println("este pornita? "+lampaExterioara.ONsauOFF());
+        LampaInterioara lampaInterioara = new LampaInterioara();
+lampaInterioara.setTipBec("LED");
+lampaInterioara.setAreAbajur(true);
+lampaInterioara.setNivelLuminozitate(5);
+System.out.println(lampaInterioara);
+
+lampaInterioara.porneste();
+System.out.println("Este pornită? " + lampaInterioara.ONsauOFF());
     }
+    
+    Prelungitor prelungitor = new Prelungitor();
+prelungitor.setNumarPrize(4);
+prelungitor.setLungimeCablu(3.5);
+prelungitor.setProtectieSupratensiune(true);
+prelungitor.setTipConector("Schuko");
+prelungitor.setPutereMaxima(2500);
+
+System.out.println(prelungitor);
+
+if (prelungitor.getProtectieSupratensiune()) {
+    System.out.println("Prelungitorul are protecție la supratensiune activată.");
+  }
 }

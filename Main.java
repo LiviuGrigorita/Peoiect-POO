db8d14c53f23abc5952451e83632b55c3c445f57
LiviuGrigorita <liviugaby1@gmail.com>
2024-12-02 13:22:09 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -3,32 +3,69 @@ package com.mycompany.proiect_java;
 public class Proiect_java {
 
     public static void main(String[] args) {
-        Lampa l1= new Lampa();
-        System.out.println(l1);
-        LampaInterioara e1=new LampaInterioara();
-        System.out.println(e1);
-        Prelungitor s1=new Prelungitor();
-        System.out.println(s1);
+        Lampa l1 = new Lampa();
+        LampaInterioara lampain = new LampaInterioara("25f", 23, 15, "rosie", true, true, false, 2, "Textil", true);
         
+        System.out.println(l1);
+        LampaInterioara e1 = new LampaInterioara();
+        System.out.println(e1);
+        SursaIluminat s1 = new SursaIluminat();
+        System.out.println(s1);
+
         LampaInterioara lampaInterioara = new LampaInterioara();
-lampaInterioara.setTipBec("LED");
-lampaInterioara.setAreAbajur(true);
-lampaInterioara.setNivelLuminozitate(5);
-System.out.println(lampaInterioara);
+        lampaInterioara.setTipAbajur("Sticlă");
+        lampaInterioara.setDimmable(true);
+        System.out.println(lampaInterioara);
 
-lampaInterioara.porneste();
-System.out.println("Este pornită? " + lampaInterioara.ONsauOFF());
-    }
+        lampaInterioara.setPornita(true);
+        l1.setpornita(true);
+
+        System.out.println("Este pornită? " + l1.ONsauOFF());
+        System.out.println("Este pornită? " + lampaInterioara.ONsauOFF());
+
+        
+        Object[] lampi = new Object[30];
+        for (int i = 0; i < 10; i++) {
+            lampi[i] = new Lampa(10 + i, "Seria" + i, 23 + i, "Maro", true);
+        }
+        for (int i = 10; i < 20; i++) {
+            lampi[i] = new LampaInterioara("25f", 13 + i, 1 + i, "rosie", true, true, false, 2, "Plastic", false);
+        }
+        for (int i = 20; i < 30; i++) {
+            lampi[i] = new SursaIluminat("26f", 5 + i, 1 + i, 30 + i, "Serie" + i, 40 - i, "rosie", false, "Priza", "calda");
+        }
+
+        
+        for (int i = 0; i <= 29; i++) {
+            System.out.println(lampi[i]);
+        }
+
     
-    Prelungitor prelungitor = new Prelungitor();
-prelungitor.setNumarPrize(4);
-prelungitor.setLungimeCablu(3.5);
-prelungitor.setProtectieSupratensiune(true);
-prelungitor.setTipConector("Schuko");
-prelungitor.setPutereMaxima(2500);
-System.out.println(prelungitor);
+        System.out.println(lampain);
 
-if (prelungitor.getProtectieSupratensiune()) {
-    System.out.println("Prelungitorul are protecție la supratensiune activată.");
-  }
+        LampaInterioara lam1 = new LampaInterioara(lampain);
+        System.out.println(lam1);
+
+        System.out.println("---------------------------------------------------------------------");
+
+    
+        for (Object obj : lampi) {
+            if (obj instanceof Lampa) {
+                Lampa lampa = (Lampa) obj;
+                if (lampa.getInaltime() > 10 && lampa.getcuBaterie()) {
+                    System.out.println("Lampa: " + lampa);
+                }
+            } else if (obj instanceof LampaInterioara) {
+                LampaInterioara lampaInterioaraObj = (LampaInterioara) obj;
+                if (lampaInterioaraObj.isPornita() && lampaInterioaraObj.isDimmable()) {
+                    System.out.println("LampaInterioara: " + lampaInterioaraObj);
+                }
+            } else if (obj instanceof SursaIluminat) {
+                SursaIluminat sursaIluminat = (SursaIluminat) obj;
+                if (sursaIluminat.getputere() > 50 && sursaIluminat.getdurata_lumina() > 100) {
+                    System.out.println("SursaIluminat: " + sursaIluminat);
+                }
+            }
+        }
+    }
 }
